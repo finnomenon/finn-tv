ddba0f3362303ace86f4f8b4df6e3d2f31d69b21
hangout <hangout@falbala.skrrl.org>
2012-01-24 01:30:58 +0100

    NZBMatrix abstracted

--- finn_tv.rb
+++ finn_tv.rb
@@ -1,24 +1,49 @@
 require "rubygems"
 require "mechanize"
 
-# "http://nzbmatrix.com/account-login.php"
-
-cookies = []
-
 unless !!ENV["NZBM_USER"] and !!ENV["NZBM_PASS"]
   puts "please specify NZBM_USER/PASS"
   exit!
 end
 
-%x{curl -s -i -F "username=#{ENV["NZBM_USER"]}" -F "password=#{ENV["NZBM_PASS"]}" "http://nzbmatrix.com/account-login.php"}.split("\n").each do |line|
-  if line.include?("Set-Cookie")
-    cookies.push(line[12..-1])
+class NZBMatrixClient
+
+  NZBMatrixURL = "http://nzbmatrix.com/"
+  NZBMatrixLoginPath = "account-login.php"
+
+  def initialize
+    @cookies = []
   end
+
+  def authenticate!(user,pass)
+    header = send_request(NZBMatrixLoginPath, {"username" => user, "password" => pass}, "-i")
+
+    header.split("\n").each do |line|
+      if line.include?("Set-Cookie")
+        @cookies.push(line[12..-1])
+      end
+    end
+
+    @cookies.length == 2
+  end
+
+private
+
+  def send_request(path,params,curlopts)
+    params_part = params.map{ |key, value| "-F \"#{key}=#{value}\"" }.join(" ")
+    %x{curl -s #{curlopts} #{params_part} "#{NZBMatrixURL}#{path}"}
+  end
+
 end
 
-if cookies.length == 2
-  puts "Logged in!"
-else
-  puts "Not logged in!"
+nzbm_client = NZBMatrixClient.new
+logged_in = nzbm_client.authenticate!(ENV["NZBM_USER"], ENV["NZBM_PASS"])
+
+puts  "Logged in? - " + logged_in.inspect
+if !logged_in
   exit!
 end
+
+
+#    nzbm_client.get_latest_shows
+
